e84c3cfbff0d94bb766f86145aad635711fd5cde
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-09-22 18:24:22 +0800

    applied InputFields component

--- app/sign_up.tsx
+++ app/sign_up.tsx
@@ -3,7 +3,7 @@ import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { CTAButton } from '@/components/CTAButton';
-import { Feather, FontAwesome } from '@expo/vector-icons';
+import { AuthInputFields } from '@/components/InputFields';
 import React, { useState } from "react";
 import { TextInput } from "react-native-paper";
 import {
@@ -18,9 +18,9 @@ import firestore from "@react-native-firebase/firestore";
 import { goToInterface } from './utils';
 
 export default function SignUp() {
-  const [username, setUsername] = useState<string | undefined>();
-  const [email, setEmail] = useState<string | undefined>();
-  const [password, setPassword] = useState<string | undefined>();
+  const [username, setUsername] = useState<string>('');  // changed from undefined to empty strings to ensure variables are always strings
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>(''); 
   const [loading, setLoading] = useState(false);
 
   const current_date = Date().toString();
@@ -92,6 +92,28 @@ export default function SignUp() {
 
     Alert.alert('Sign Up Error', message);
   };
+
+  const fields = [
+    {
+      placeholder: "Username",
+      value: username,
+      onChangeText: setUsername,
+      icon: { library: "Feather" as 'Feather', name: "user" },
+    },
+    {
+      placeholder: "Email",
+      value: email,
+      onChangeText: setEmail,
+      icon: { library: "FontAwesome" as 'FontAwesome', name: "envelope-o" },
+    },
+    {
+      placeholder: "Password",
+      value: password,
+      onChangeText: setPassword,
+      secureTextEntry: true,
+      icon: { library: "Feather" as 'Feather', name: "lock" },
+    },
+  ];
  
   return (
     <ParallaxScrollView
@@ -106,56 +128,18 @@ export default function SignUp() {
         <ThemedText type="title">Welcome!</ThemedText>
       </ThemedView>
 
-      <ThemedView style={styles.stepContainer}>
-        <View style={styles.inputWrapper}>
-          <Feather name="user" style={styles.icon} />
-          <TextInput 
-              placeholder='Username'
-              value={username}
-              onChangeText={setUsername}
-              mode='flat'
-              underlineColor='#ccc' 
-              activeUnderlineColor= '#407F3D'
-              style={styles.input}
-          />
-        </View>
-
-        <View style={styles.inputWrapper}>
-          <FontAwesome name="envelope-o" style={styles.icon} />
-          <TextInput 
-            placeholder='Email'
-            value={email}
-            onChangeText={setEmail}
-            mode='flat'
-            underlineColor='#ccc' 
-            activeUnderlineColor= '#407F3D'
-            style={styles.input}
-          />
-        </View>
-
-        <View style={styles.inputWrapper}>
-          <Feather name="lock" style={styles.icon} />
-          <TextInput
-            placeholder='Password'
-            value={password}
-            onChangeText={setPassword}
-            mode='flat'
-            secureTextEntry
-            underlineColor='#ccc' 
-            activeUnderlineColor= '#407F3D'
-            style={styles.input}
-          />
-        </View>
+      <ThemedView>
+        <AuthInputFields fields={fields}/>
 
         <CTAButton
-            title={loading ? "Signing Up..." : "Sign Up"}
-            onPress={handleSignUp}
-            variant="primary"
+          title={loading ? "Signing Up..." : "Sign Up"}
+          onPress={handleSignUp}
+          variant="primary"
         />
         <CTAButton
-            title="Log In"
-            onPress={() => router.push('/login')}
-            variant="secondary"
+          title="Log In"
+          onPress={() => router.push('/login')}
+          variant="secondary"
         />          
       </ThemedView>
     </ParallaxScrollView>
@@ -168,41 +152,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     gap: 8,
   },
-  stepContainer: {
-    gap: 12,
-    marginBottom: 12,
-    borderRadius: 10,
-    overflow: 'hidden',
-  },
   reactLogo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
     position: 'absolute',
-  },
-  inputWrapper:{
-    flexDirection: 'row',
-    alignItems: 'center',
-    position: 'relative', 
-    borderRadius: 10,            
-    overflow: 'hidden',
-    height:50,
-  },
-  input:{
-    backgroundColor: '#F6F5F3',
-    paddingLeft: 40,
-    flex:1,
-    borderRadius: 10,
-    overflow: 'hidden',
-    fontSize: 14,
-    color: '#757575'
-  },
-  icon: {
-    position: 'absolute',
-    paddingLeft: 20,
-    zIndex: 10,
-    color: '#757575',
-    fontSize: 24,
   },
 });
