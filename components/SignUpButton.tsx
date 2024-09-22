bb8681e6145f97960e5f7c4886987452c24f64c6
beatrizlyle <beatriz.lyle@gmail.com>
2024-09-22 15:05:00 +0800

    created signupbutton component

---
+++ components/SignUpButton.tsx
@@ -0,0 +1,29 @@
+import React, { FC } from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+type ButtonType = "primary" | "secondary";
+
+interface SignUpButtonProps {
+  title: string;
+  variant: ButtonType;
+  onPress: () => void;
+}
+
+export const SignUpButton: FC<SignUpButtonProps> = ({ title, onPress, variant }) => {
+    const containerStyle =
+      variant === "primary" 
+        ? "h-12 bg-green-700 rounded-lg justify-center items-center" 
+        : "h-12 bg-white rounded-lg justify-center items-center";
+  
+    const textStyle =
+      variant === "primary"
+        ? "text-white text-base" 
+        : "text-black text-base";
+  
+
+  return (
+    <TouchableOpacity onPress={onPress} className={containerStyle}>
+      <Text className={textStyle}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
