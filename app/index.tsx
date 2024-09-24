d69b8d55c1df3b7edceb4ec5845b9e84acfa1c84
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-09-24 11:39:03 +0800

    updated index

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,11 @@
-import { Image, StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 import React, {useState, useEffect} from 'react';
 import { router, Link } from 'expo-router';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { CTAButton } from '@/components/CTAButton';
 import { goToInterface } from './utils';
+import { TitleComponent } from '@/components/Title';
+import { LoginButton } from '@/components/LoginButton';
+import { SignUpButton } from '@/components/SignUpButton';
 
 export default function Index() {
 
@@ -14,47 +14,21 @@ export default function Index() {
   }, [router]);
 
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <ThemedView className="flex-1 justify-center">
+      <TitleComponent />
+
+     <View className="w-full px-8 mt-20">
+        <LoginButton
+          title="Log In"
+          onPress={() => router.push('/login')}
+          variant="primary"
         />
-      }>
-    <ThemedView style={styles.stepContainer}>
-      <CTAButton
-        title="Log In"
-        onPress={() => router.push('/login')}
-        variant="primary"
-      />
-      <CTAButton
-        title="Sign Up"
-        onPress={() => router.push('/sign_up')}
-        variant="primary"
-      />
+        <SignUpButton
+          title="Sign Up"
+          onPress={() => router.push('/sign_up')}
+          variant="primary"
+        />
+      </View> 
     </ThemedView>
-    <View></View>
-    </ParallaxScrollView>
   );
 }
-
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
