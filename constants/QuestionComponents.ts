fa22d941b0ad173e953a402f1f7e457afa229e27
Angela <angela.delapena06@gmail.com>
2024-10-09 12:39:03 +0800

    question components listed separately

---
+++ constants/QuestionComponents.ts
@@ -0,0 +1,5 @@
+import Transportation1 from "@/app/(quiz)/Transportation1";
+import Transportation2 from "@/app/(quiz)/Transportation2";
+import Transportation3 from "@/app/(quiz)/Transportation3";
+
+export const components = [Transportation1, Transportation2, Transportation3];  // Add more components if needed
