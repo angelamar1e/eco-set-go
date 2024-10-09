ae8fccf1d06968cc6fd63fcdb0fba40cf76340e3
Angela <angela.delapena06@gmail.com>
2024-10-09 12:36:20 +0800

    fixed template 2, 3

--- app/components/quiz/Template2.tsx
+++ app/components/quiz/Template2.tsx
@@ -1,42 +1,46 @@
-import React, { FC,useState} from 'react';
-import { View} from 'react-native';
-import { ThemedView } from '@/components/ThemedView';
-import { ThemedText } from '@/components/ThemedText';
-import { QuestionContainer } from './QuestionContainer';
-import { SuggestedAnswers } from './SuggestedAnswers';
-import { TextField } from './TextField';
-import { NavigationButtons } from './NavigationButtons';
+import React, { FC, useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from "@/components/ThemedText";
+import { QuestionContainer } from "./QuestionContainer";
+import { PresetChoices } from "./PresetChoices";
+import { TextField } from "./TextField";
+import { NavigationButtons } from "./NavigationButtons";
 
 interface Template2Props {
-    category: string;
-    question: string;
-    answers: string[];
-    textFieldLabel: string;
-    onNext: () => void;         
-    onBack?: () => void;        
-    showBackButton?: boolean;  
-  }
+  category: string;
+  question: string;
+  choices?: Map<string, number>;
+  textFieldLabel: string;
+  defaultValue: number;
+  onNext: () => void;
+  onBack?: () => void;
+  showBackButton?: boolean;
+  onAnswer: (answer: number) => void; 
+}
 
 export const Template2: FC<Template2Props> = ({
-    category,
-    question,
-    answers,
-    textFieldLabel,
-    onNext,
-    onBack,
-    showBackButton = true, 
+  category,
+  question,
+  choices: choices,
+  textFieldLabel,
+  defaultValue,
+  onNext,
+  onBack,
+  showBackButton = true,
+  onAnswer
 }) => {
- 
-  // State to manage selected answer
-  const [selectedAnswer, setSelectedAnswer] = useState<string | null>(null);
+  // State to manage selected answer from preset choices
+  const [answer, setAnswer] = useState<number>();
 
   // State to manage the input value in the TextField
-  const [inputValue, setInputValue] = useState<string>("");
+  const [inputValue, setInputValue] = useState<string>(defaultValue.toString());
 
   // Function to handle answer selection
-  const handlePress = (answer: string) => {
-    setSelectedAnswer(answer);
-    setInputValue(answer);
+  const handlePress = (answer: number) => {
+    setAnswer(answer);
+    setInputValue(answer.toString());
+    onAnswer(answer);
   };
 
   // Function to handle text input change
@@ -44,54 +48,66 @@ export const Template2: FC<Template2Props> = ({
     setInputValue(text);
   };
 
-    return (
-      <ThemedView className="flex-1 px-6">
-        <QuestionContainer>
-            <ThemedText type='defaultSemiBold' className='text-lime-800 mb-3'>{category}</ThemedText>
-            <ThemedText type="default" className='text-black text-[20px] mb-3'>{question}</ThemedText>
-
-            {/* Suggested Answers */}
-            <View className='flex-row flex-wrap justify-left mb-10'>
-                {answers.map((answer) => (
-                <SuggestedAnswers
-                    key={answer}
-                    title={answer}
-                    isSelected={selectedAnswer === answer}
-                    onPress={() => handlePress(answer)}
-                />
-                ))}
-            </View>
-
-            {/* Text Fields */}
-            <View className='ml-2 mb-5'>
-              <TextField
-                label={textFieldLabel}
-                value={inputValue}
-                onChangeText={handleTextChange}
-              />
-            </View>
-
-            {/* Navigation Button */}
-            <View className='flex-row justify-center mt-4'>
-              {showBackButton && (
-                <NavigationButtons
-                  title="Back"
-                  variant="secondary"
-                  onPress={onBack}
-                />
-              )}
-                <NavigationButtons
-                  title="Next"
-                  variant="primary"
-                  onPress={() => {
-                  console.log('Next button pressed');
-                  onNext();
-              }}
-                />
-            </View>
-        </QuestionContainer>
-      </ThemedView>
-    );
+  const handleBlur = () => {
+    onAnswer(parseInt(inputValue));
   };
-  
+
+  return (
+    <ThemedView className="flex-1 px-6">
+      <QuestionContainer>
+        <ThemedText type="defaultSemiBold" className="text-lime-800 mb-3">
+          {category}
+        </ThemedText>
+        <ThemedText type="default" className="text-black text-[20px] mb-3">
+          {question}
+        </ThemedText>
+
+        <View className="flex-row flex-wrap justify-left mb-10">
+          {choices ? (
+            Object.entries(choices).map(([key, value]) => (
+              <PresetChoices
+                key={key}
+                title={key}
+                isSelected={answer || answer == 0 ? answer === value : value === defaultValue}
+                onPress={() => handlePress(value)}
+              />
+            ))
+          ) : (
+            <Text> Loading... </Text>
+          )}
+        </View>
+
+        {/* Text Input Field */}
+        <View className="ml-2 mb-5">
+          <TextField
+            label={textFieldLabel}
+            value={inputValue}
+            onChangeText={handleTextChange}
+            onBlur={handleBlur}
+          />
+        </View>
+
+        {/* Navigation Button */}
+        <View className="flex-row justify-center mt-4">
+          {showBackButton && (
+            <NavigationButtons
+              title="Back"
+              variant="secondary"
+              onPress={onBack}
+            />
+          )}
+          <NavigationButtons
+            title="Next"
+            variant="primary"
+            onPress={() => {
+              console.log("Next button pressed");
+              onNext();
+            }}
+          />
+        </View>
+      </QuestionContainer>
+    </ThemedView>
+  );
+};
+
 export default Template2;
--- app/components/quiz/Template3.tsx
+++ app/components/quiz/Template3.tsx
@@ -1,80 +1,95 @@
-import { useState, FC } from 'react';
-import { View } from 'react-native';
-import { QuestionContainer } from '@/app/components/quiz/QuestionContainer';
-import { NavigationButtons } from '@/app/components/quiz/NavigationButtons';
-import { ThemedText } from '@/components/ThemedText';
-import { RadioChoices } from '@/app/components/quiz/RadioChoices';
-import { ThemedView } from '@/components/ThemedView';
+import { useState, FC } from "react";
+import { View } from "react-native";
+import { QuestionContainer } from "@/app/components/quiz/QuestionContainer";
+import { NavigationButtons } from "@/app/components/quiz/NavigationButtons";
+import { ThemedText } from "@/components/ThemedText";
+import { RadioChoices } from "@/app/components/quiz/RadioChoices";
+import { ThemedView } from "@/components/ThemedView";
+import { Text } from "react-native-paper";
 
-interface Template3Props {
-    category: string;
-    question: string;
-    answer: string[];
-    onNext: () => void;         
-    onBack?: () => void;        
-    showBackButton?: boolean;
+interface Template3BaseProps {
+  category: string;
+  question: string;
+  onNext: () => void;
+  onBack?: () => void;
+  showBackButton?: boolean;
+  onAnswer: (answer: string | number) => void;
 }
 
+interface stringChoiceValues extends Template3BaseProps {
+  choices: Map<string, string>;
+  defaultValue: string;
+}
+
+interface numberChoiceValues extends Template3BaseProps {
+  choices: Map<string, number>;
+  defaultValue: number;
+}
+
+type Template3Props = stringChoiceValues | numberChoiceValues;
+
 export const Template3: FC<Template3Props> = ({
-        category,
-        question,
-        answer,
-        onNext,
-        onBack,
-        showBackButton = true, 
-    }) => 
-        
-    {
-        const [selectedChoice, setSelectedChoice] = useState<number | null>(null);
+  category,
+  question,
+  choices,
+  defaultValue,
+  onNext,
+  onBack,
+  showBackButton = true,
+  onAnswer
+}) => {
+  const [answer, setAnswer] = useState<string | number>();
 
-        const handleChoiceSelect = (choiceId: number) => {
-          setSelectedChoice(choiceId);
-      
-        };
-    
-        return (
-            <ThemedView className="flex-1 px-6">
-            <QuestionContainer>
-                <ThemedText type='defaultSemiBold' className='text-lime-800 mb-3' >{category}</ThemedText>
-                <ThemedText type="default" className='text-black text-[20px] mb-3'>
-                    {question}
-                </ThemedText>
-                
-                {/* Radio Chouces */}
-                <View className="flex-wrap flex-row justify-center mt-10 mb-3">
-                    {answer.map((answer, index) => (
-                        <RadioChoices
-                            key={index}
-                            title={answer}
-                            isSelected={selectedChoice === index + 1} 
-                            onPress={() => handleChoiceSelect(index + 1)}
-                        />
-                    ))}
-                </View>
+  const handlePress = (answer: string | number) => {
+    setAnswer(answer);
+    onAnswer(answer);
+  };
 
-                {/* Navigation Button */}
-                <View className='flex-row justify-center mt-4'>
-                {showBackButton && (
-                    <NavigationButtons
-                        title="Back"
-                        variant="secondary"
-                        onPress={onBack}
-                    />
-                )}
-                    <NavigationButtons
-                        title="Next"
-                        variant="primary"
-                        onPress={() => {
-                        console.log('Next button pressed');
-                        onNext();
-                }}
-                    />
-                </View>
-            
-            </QuestionContainer>
-        
-        </ThemedView>
+  return (
+    <ThemedView className="flex-1 px-6">
+      <QuestionContainer>
+        <ThemedText type="defaultSemiBold" className="text-lime-800 mb-3">
+          {category}
+        </ThemedText>
+        <ThemedText type="default" className="text-black text-[20px] mb-3">
+          {question}
+        </ThemedText>
 
-        );
+        {/* Radio Choices */}
+        <View className="flex-wrap flex-row justify-center mt-10 mb-3">
+          {choices ? (
+            Object.entries(choices).map(([key, value]) => (
+              <RadioChoices
+                key={key}
+                title={key}
+                isSelected={answer ? answer === value : value === defaultValue}
+                onPress={() => handlePress(value)}
+              />
+            ))
+          ) : (
+            <Text> Loading... </Text>
+          )}
+        </View>
 
-    };
+        {/* Navigation Button */}
+        <View className="flex-row justify-center mt-4">
+          {showBackButton && (
+            <NavigationButtons
+              title="Back"
+              variant="secondary"
+              onPress={onBack}
+            />
+          )}
+          <NavigationButtons
+            title="Next"
+            variant="primary"
+            onPress={() => {
+              console.log("Next button pressed");
+              onNext();
+            }}
+          />
+        </View>
+      </QuestionContainer>
+    </ThemedView>
+  );
+};
