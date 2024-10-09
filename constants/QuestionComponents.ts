cbc14db5bb66e9a6742bdda36f2265bf1a6a63e3
Angela <angela.delapena06@gmail.com>
2024-10-09 19:02:37 +0800

    listed new question components for quiz

--- constants/QuestionComponents.ts
+++ constants/QuestionComponents.ts
@@ -1,5 +1,31 @@
 import Transportation1 from "@/app/(quiz)/Transportation1";
+import Transportation10 from "@/app/(quiz)/Transportation10";
+import Transportation11 from "@/app/(quiz)/Transportation11";
+import Transportation12 from "@/app/(quiz)/Transportation12";
+import Transportation13 from "@/app/(quiz)/Transportation13";
+import Transportation14 from "@/app/(quiz)/Transportation14";
 import Transportation2 from "@/app/(quiz)/Transportation2";
 import Transportation3 from "@/app/(quiz)/Transportation3";
+import Transportation4 from "@/app/(quiz)/Transportation4";
+import Transportation5 from "@/app/(quiz)/Transportation5";
+import Transportation6 from "@/app/(quiz)/Transportation6";
+import Transportation7 from "@/app/(quiz)/Transportation7";
+import Transportation8 from "@/app/(quiz)/Transportation8";
+import Transportation9 from "@/app/(quiz)/Transportation9";
 
-export const components = [Transportation1, Transportation2, Transportation3];  // Add more components if needed
+export const components = [
+  Transportation1,
+  Transportation2,
+  Transportation3,
+  Transportation4,
+  Transportation5,
+  Transportation6,
+  Transportation7,
+  Transportation8,
+  Transportation9,
+  Transportation10,
+  Transportation11,
+  Transportation12,
+  Transportation13,
+  Transportation14,
+]; // Add more components if needed
