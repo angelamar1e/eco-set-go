2cbdc0707f893e6a6176bbf7bf66fcda28d5e45b
Angela <angela.delapena06@gmail.com>
2024-10-13 01:13:45 +0800

    updated quiz. template props

--- types/QuestionData.ts
+++ types/QuestionData.ts
@@ -1,11 +1,9 @@
 export interface QuestionData {
     variable: string;
     question: string;
-    choices?: Map<string, string> & Map<string, number>;
+    choices?: any;
     category: string;
-    default_value: string | number | boolean;
     input_label?: string;
     template: number;
     stepperTitles?: string[];
-    checkboxes?: string[];
 }
--- types/QuizProps.ts
+++ types/QuizProps.ts
@@ -1,23 +1,12 @@
-import { CheckboxProps } from "react-native-paper";
-
-export interface QuestionProps {
-    question: string; // The question as a string
-    choices?: Map<string, string> | Map<string, number>; // The choices as a Map
-}
-
 export interface TemplateProps {
     category: string;
     inputLabel?: string;
     question: string;
     choices?: Map<string, string> | Map<string, number>;
-    defaultValue: string | number | boolean;
-    onAnswer: (answer: string | number) => void;
+    defaultValue: any
+    onAnswer: (answer: string | number | string[]) => void;
 }
 
-export interface StepperProps extends TemplateProps {
+export interface StepperTemplateProps extends TemplateProps {
     stepperTitle?: string[];
-} 
-
-export interface CheckboxesProps extends TemplateProps {
-    checkboxes?: string[];
 }
