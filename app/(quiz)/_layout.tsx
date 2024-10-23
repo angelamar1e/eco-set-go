9f1a0ae0b27076598ffd3e282956df1677d0cb05
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-10-23 18:03:26 +0800

    Quiz Header updated with UI Kitten

--- app/(quiz)/_layout.tsx
+++ app/(quiz)/_layout.tsx
@@ -2,48 +2,52 @@ import { Stack } from "expo-router";
 import { QuizDocsProvider } from "@/contexts/QuizContext";
 import { EmissionsProvider } from "@/contexts/EmissionsContext";
 import Calculator from "../components/quiz/Calculator";
-import { SafeAreaView, View, TouchableOpacity } from "react-native";
-import { Ionicons } from "@expo/vector-icons";
+import { SafeAreaView, View } from "react-native";
 import { useNavigation } from '@react-navigation/native';
+import { useTheme, Layout, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
+import { Ionicons } from '@expo/vector-icons'; 
+import { myTheme } from "@/constants/custom-theme";
 
-function QuizHeader() {
+const QuizHeader = () => {
   const navigation = useNavigation();
+  const theme = useTheme();
+
+  const navigateBack = () => navigation.goBack();
+  const navigateToList = () => navigation.navigate('components/quiz/ListScreen' as never);
+
+  const BackIcon = () => <Ionicons name="chevron-back" size={35} color={theme['color-primary-900']} />;
+  const ListIcon = () => <Ionicons name="list" size={35} color={theme['color-primary-900']} />;
 
   return (
-    <View className="h-1/8 bg-white border-b border-stone-300 flex-row pl-5">
-      <TouchableOpacity onPress={() => navigation.goBack()}>
-        <Ionicons name="chevron-back" size={35} color="#78716C" 
-          style={{ flex: 1, alignItems: 'center', paddingTop: 20, paddingRight: 15 }}
-        />
-      </TouchableOpacity>
-
-      <View className="pt-2"><Calculator /></View>
-
-      {/* 'as never' to avoid error temporarily */}
-      <TouchableOpacity onPress={() => navigation.navigate('components/quiz/ListScreen' as never)}>
-        <Ionicons name='list' size={35} color="#78716C" 
-          style={{alignItems: 'center', paddingTop: 20, paddingLeft: 172}}
-        />
-      </TouchableOpacity>
-    </View>
+    <Layout level='1' style={{ borderBottomWidth: 1, borderBottomColor: myTheme["color-primary-900"], borderStyle: 'solid' }}>
+      <TopNavigation
+        accessoryLeft={() => (
+          <View className='flex-row items-center'>
+            <TopNavigationAction icon={BackIcon} onPress={navigateBack} />
+            <Calculator />
+          </View> 
+        )}
+        accessoryRight={() => (
+          <TopNavigationAction icon={ListIcon} onPress={navigateToList} />
+        )}
+      />
+    </Layout>
   );
-}
+};
 
-function Quiz() {
-  return (
-    <SafeAreaView className="flex-1 pt-10">
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            headerShown: true,
-            header: () => <QuizHeader />,
-          }}
-        />
-      </Stack>
-    </SafeAreaView>
-  );
-}
+const Quiz = () => (
+  <SafeAreaView className="flex-1 pt-10">
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          headerShown: true,
+          header: () => <QuizHeader />,
+        }}
+      />
+    </Stack>
+  </SafeAreaView>
+);
 
 export default function QuizLayout() {
   return (
--- app/components/quiz/Calculator.tsx
+++ app/components/quiz/Calculator.tsx
@@ -1,19 +1,33 @@
-import { getUserUid } from "@/app/utils/utils";
-import React, { useContext, useEffect, useState } from "react";
-import firestore from "@react-native-firebase/firestore";
-import { Text } from "react-native-paper";
-import { EmissionsContext, EmissionsProvider } from "@/contexts/EmissionsContext";
-import { ThemedText } from "@/components/ThemedText";
+import React, { useContext } from "react";
+import { EmissionsContext } from "@/contexts/EmissionsContext";
+import { Text, Layout } from "@ui-kitten/components";
+import { styled } from 'nativewind';
+import { myTheme } from "@/constants/custom-theme";
+
+const StyledTextLarge = styled(Text);
+const StyledTextSmall = styled(Text);
+const StyledLayout = styled(Layout);
 
 const Calculator: React.FC = () => {
-  const { overallFootprint } = useContext(EmissionsContext); // Get carEmissions from context
+  const { overallFootprint } = useContext(EmissionsContext); 
 
   return (
-    <ThemedText type='defaultSemiBold' className="pt-6 text-5xl text-lime-500">
-      {overallFootprint.toFixed(2)}
-      <ThemedText type='default' className="text-stone-500 text-3sm"> tons{"\n"}   of CO2e per year
-      </ThemedText>
-    </ThemedText>
+    <StyledLayout className="flex-row items-center justify-center shadow p-2">
+      <StyledTextLarge
+        category='h1' 
+        style={{ color: myTheme["color-primary-700"] }}
+        className="ml-2"
+      >
+        {overallFootprint.toFixed(2)}
+      </StyledTextLarge>
+      <StyledTextSmall
+        category="p1"
+        style={{ color: myTheme["color-primary-900"] }} 
+        className="ml-2" 
+      >
+        tons{"\n"}of CO2e per year
+      </StyledTextSmall>
+    </StyledLayout>
   );
 };
 
