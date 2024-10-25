3c7d9b858637af315e9e60aa187659abfa1116ff
beatrizlyle <beatriz.lyle@gmail.com>
2024-10-25 14:53:50 +0800

    edited preferences, setting page

--- app/components/(tabs)/Profile and Settings/Preferences.tsx
+++ app/components/(tabs)/Profile and Settings/Preferences.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ScrollView } from 'react-native';
-import { Toggle, Input, Text, Layout } from '@ui-kitten/components';
+import { Toggle, Input, Text, Layout, Divider } from '@ui-kitten/components';
 import { styled } from 'nativewind';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -8,6 +8,7 @@ const StyledText = styled(Text);
 const StyledLayout = styled(Layout);
 const StyledInput = styled(Input);
 const StyledToggle = styled(Toggle);
+const StyledDivider = styled(Divider);
 
 
 interface PreferencesProps {
@@ -27,50 +28,40 @@ const Preferences: React.FC<PreferencesProps> = ({
   newsletter,
   setNewsletter,
 }) => (
-  <ScrollView className="px-4">
-    <StyledText className="text-lg font-bold text-green-800 mt-4">
-      Push notifications
-    </StyledText>
-    <StyledText className="text-gray-600">
-      Stay up to date with all things happening in your app
-    </StyledText>
-    <Toggle
-      checked={pushNotifications}
-      onChange={setPushNotifications}
-      className="my-4"
-    />
+  <StyledLayout className="m-2 p-2">
+    <StyledLayout className='mt-4 ml-2 mr-2 p-1 flex flex-row'>
+      <StyledText category='h6'>Push notifications</StyledText>
+      <StyledToggle
+        className='ml-auto'
+        checked={pushNotifications}
+        onChange={setPushNotifications}
+      />
+    </StyledLayout>
+    
+    <StyledDivider className='ml-2 mr-2'></StyledDivider>
 
-    <StyledText className="text-lg font-bold text-green-800 mt-4">
-      Action reminders
-    </StyledText>
-    <StyledText className="text-gray-600">
-      Handy nudges to track your carbon-busting actions
-    </StyledText>
-    <Toggle
-      checked={actionReminders}
-      onChange={setActionReminders}
-      className="my-4"
-    />
-    <Input
-      placeholder="Mon, Wed, Fri, Sat"
-      value="16:30"
-      accessoryRight={<Ionicons name="chevron-forward-outline" />}
-      className="my-2"
-    />
+    <StyledLayout className='mb-4 mt-4 ml-2 mr-2 p-1 flex flex-row'>
+      <StyledText category='h6'>Action reminders</StyledText>
+      <StyledToggle
+        className='ml-auto'
+        checked={actionReminders}
+        onChange={setActionReminders}
+      />
+    </StyledLayout>
 
-    <StyledText className="text-lg font-bold text-green-800 mt-4">
-      Newsletter
-    </StyledText>
-    <StyledText className="text-gray-600">
-      Our email, full of tips, tricks, and updates
-    </StyledText>
-    <Toggle
-      checked={newsletter}
-      onChange={setNewsletter}
-      className="my-4"
-    />
+    <StyledDivider className='ml-2 mr-2'></StyledDivider>
 
-  </ScrollView>
+    <StyledLayout className='mb-4 mt-4 ml-2 mr-2 p-1 flex flex-row'>
+      <StyledText category='h6'>Action notifications</StyledText>
+      <StyledToggle
+        className='ml-auto'
+        checked={actionReminders}
+        onChange={setActionReminders}
+      />
+    </StyledLayout>
+
+  
+  </StyledLayout>
 );
 
 export default Preferences;
