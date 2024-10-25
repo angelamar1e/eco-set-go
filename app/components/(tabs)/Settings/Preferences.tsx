7704f6aa684f1c77e726ad686f57bdcc006bcf56
beatrizlyle <beatriz.lyle@gmail.com>
2024-10-25 21:52:46 +0800

    added select frequency on action reminders

--- app/components/(tabs)/Settings/Preferences.tsx
+++ app/components/(tabs)/Settings/Preferences.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
-import { Toggle, Input, Text, Layout, Divider } from '@ui-kitten/components';
+import React, { useState } from 'react'; 
+import { Toggle, Text, Layout, Divider, Select, SelectItem, IndexPath } from '@ui-kitten/components';
 import { styled } from 'nativewind';
 
 const StyledText = styled(Text);
 const StyledLayout = styled(Layout);
 const StyledToggle = styled(Toggle);
 const StyledDivider = styled(Divider);
-
+const StyledSelect = styled(Select);
+const StyledSelectItem = styled(SelectItem);
 
 interface PreferencesProps {
   pushNotifications: boolean;
@@ -20,30 +21,66 @@ const Preferences: React.FC<PreferencesProps> = ({
   setPushNotifications,
   actionReminders,
   setActionReminders,
-}) => (
-  <StyledLayout className="m-2 p-2">
-    <StyledLayout className='mt-4 ml-2 mr-2 p-1 flex flex-row'>
-      <StyledText category='h6'>Push notifications</StyledText>
-      <StyledToggle
-        className='ml-auto'
-        checked={pushNotifications}
-        onChange={setPushNotifications}
-      />
-    </StyledLayout>
-    
-    <StyledDivider className='ml-2 mr-2 mt-5'></StyledDivider>
+}) => {
+  const [selectedFrequencyIndex, setSelectedFrequencyIndex] = useState<IndexPath | null>(null);
+  const frequencyOptions = ['Once a day', 'Twice a day', 'Thrice a day'];
 
-    <StyledLayout className='mb-4 mt-4 ml-2 mr-2 p-1 flex flex-row'>
-      <StyledText category='h6'>Action reminders</StyledText>
-      <StyledToggle
-        className='ml-auto'
-        checked={actionReminders}
-        onChange={setActionReminders}
-      />
-    </StyledLayout>
+  const handleSelected = (index: IndexPath | IndexPath[]) => {
+    const selectedIndex = Array.isArray(index) ? index[0] : index;
+    setSelectedFrequencyIndex(selectedIndex);
+  };
 
-  
-  </StyledLayout>
-);
+  const selectedFrequency = selectedFrequencyIndex
+    ? frequencyOptions[selectedFrequencyIndex.row]
+    : 'Select frequency';
+
+  return (
+    <StyledLayout className="m-2 p-2">
+      <StyledLayout className="mt-4 ml-2 mr-2 p-1 flex flex-row">
+        <StyledText category="h6">Push notifications</StyledText>
+        <StyledToggle
+          className="ml-auto"
+          checked={pushNotifications}
+          onChange={setPushNotifications}
+        />
+      </StyledLayout>
+      
+      <StyledDivider className="ml-2 mr-2 mt-5" />
+
+      <StyledLayout className="mb-4 mt-4 ml-2 mr-2 p-1 flex flex-row">
+        <StyledText category="h6">Action reminders</StyledText>
+        <StyledToggle
+          className="ml-auto"
+          checked={actionReminders}
+          onChange={(value) => {
+            setActionReminders(value);
+            if (!value) {
+              setSelectedFrequencyIndex(null); 
+            }
+          }}
+        />
+      </StyledLayout>
+
+      {actionReminders && (
+        <StyledLayout className="ml-2 mr-2">
+          <StyledSelect
+            className="w-full"
+            placeholder="Select frequency"
+            value={selectedFrequency}
+            selectedIndex={selectedFrequencyIndex}
+            onSelect={handleSelected}
+          >
+            {frequencyOptions.map((option, index) => (
+              <StyledSelectItem
+                key={index}
+                title={option}
+              />
+            ))}
+          </StyledSelect>
+        </StyledLayout>
+      )}
+    </StyledLayout>
+  );
+};
 
 export default Preferences;
