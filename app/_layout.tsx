b57626a7fca8bd098a1b586d9a8b6c9978eda441
Angela <angela.delapena06@gmail.com>
2024-11-03 11:19:08 +0800

    wrapped app under new contexts

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,57 +8,69 @@ import { useColorScheme } from "react-native";
 import { EmissionsDataProvider } from "@/contexts/EmissionsData";
 import { UserProvider } from "@/contexts/UserContext";
 import { UserLogsProvider } from "@/contexts/UserLogs";
+import { UserGoalProvider } from "@/contexts/UserGoalContext";
+import { EcoActionsProvider } from "@/contexts/EcoActions";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
 
   return (
     <UserProvider>
-      <UserLogsProvider>
+      <EcoActionsProvider>
         <EmissionsProvider>
           <EmissionsDataProvider>
-            <ApplicationProvider
-              {...eva}
-              theme={
-                colorScheme === "dark"
-                  ? { ...eva.dark, ...myTheme }
-                  : { ...eva.light, ...myTheme.light }
-              }
-            >
-              <GestureHandlerRootView style={{ flex: 1 }}>
-                <Stack>
-                  <Stack.Screen name="index" options={{ headerShown: false }} />
-                  <Stack.Screen name="login" options={{ headerShown: false }} />
-                  <Stack.Screen
-                    name="sign_up"
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="(tabs)"
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="(quiz)"
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="components/quiz/ListScreen"
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="Profile"
-                    options={{ headerShown: false }}
-                  />
-                  <Stack.Screen
-                    name="components/(tabs)/Eco Articles/[article]"
-                    options={{ headerShown: false }}
-                  />
-                </Stack>
-              </GestureHandlerRootView>
-            </ApplicationProvider>
+            <UserLogsProvider>
+              <UserGoalProvider>
+                <ApplicationProvider
+                  {...eva}
+                  theme={
+                    colorScheme === "dark"
+                      ? { ...eva.dark, ...myTheme }
+                      : { ...eva.light, ...myTheme.light }
+                  }
+                >
+                  <GestureHandlerRootView style={{ flex: 1 }}>
+                    <Stack>
+                      <Stack.Screen
+                        name="index"
+                        options={{ headerShown: false }}
+                      />
+                      <Stack.Screen
+                        name="login"
+                        options={{ headerShown: false }}
+                      />
+                      <Stack.Screen
+                        name="sign_up"
+                        options={{ headerShown: false }}
+                      />
+                      <Stack.Screen
+                        name="(tabs)"
+                        options={{ headerShown: false }}
+                      />
+                      <Stack.Screen
+                        name="(quiz)"
+                        options={{ headerShown: false }}
+                      />
+                      <Stack.Screen
+                        name="components/quiz/ListScreen"
+                        options={{ headerShown: false }}
+                      />
+                      <Stack.Screen
+                        name="Profile"
+                        options={{ headerShown: false }}
+                      />
+                      <Stack.Screen
+                        name="components/(tabs)/Eco Articles/[article]"
+                        options={{ headerShown: false }}
+                      />
+                    </Stack>
+                  </GestureHandlerRootView>
+                </ApplicationProvider>
+              </UserGoalProvider>
+            </UserLogsProvider>
           </EmissionsDataProvider>
         </EmissionsProvider>
-      </UserLogsProvider>
+      </EcoActionsProvider>
     </UserProvider>
   );
 }
