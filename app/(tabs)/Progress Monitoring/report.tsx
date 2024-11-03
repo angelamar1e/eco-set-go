7aa47c101126dcb59776b03d8b6278ef3c9483fc
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-11-04 07:01:47 +0800

    progress and goal setting UI updated

--- app/(tabs)/Progress Monitoring/report.tsx
+++ app/(tabs)/Progress Monitoring/report.tsx
@@ -1,15 +1,16 @@
-import React, { useContext, useState } from "react";
-import { View, Dimensions } from "react-native";
+import React, { useContext, useEffect, useState } from "react";
+import { View, Dimensions, SectionListComponent, TouchableOpacity, Appearance } from "react-native";
 import { BarChart, StackedBarChart } from "react-native-chart-kit";
 import { styled } from "nativewind";
 // import Card from "react-native-paper";
-import { Text, Layout, Select, SelectItem, Card } from "@ui-kitten/components";
+import { Text, Layout, Card } from "@ui-kitten/components";
 import { useLogsContext } from "@/contexts/UserLogs";
 import { myTheme } from "@/constants/custom-theme";
 import GoalSetting from "@/app/components/(tabs)/Progress Monitoring/GoalSetting";
 import { conditionalConvertGramsToKg, convertTonsToGrams } from "@/app/utils/EstimationUtils";
 import { EmissionsDataContext } from "@/contexts/EmissionsData";
 import { set } from "@react-native-firebase/database";
+import { DEFAULT_X_LABELS_HEIGHT_PERCENTAGE } from "react-native-chart-kit/dist/AbstractChart";
 
 // Define types for report data
 type ReportData = {
@@ -22,16 +23,12 @@ const StyledLayout = styled(Layout);
 const StyledText = styled(Text);
 const StyledCard = styled(Card);
 
-const DataCard = ({ className = "", style = "", ...props }) => {
-  return (
-    <View className="h-full w-5/12 p-3 flex mx-2 rounded-3xl border" {...props}>
-    </View>
-  )
-}
 
 // ProgressReport component
 const ProgressReport = () => {
   const [period, setPeriod] = useState<"Daily" | "Weekly" | "Monthly">("Daily");
+  const [colorScheme, setColorScheme] = useState<"light" | "dark">(Appearance.getColorScheme() || "light");
+
   const {
     currentFootprint,
     dailyImpact,
@@ -50,39 +47,69 @@ const ProgressReport = () => {
     Monthly: monthlyImpact,
   };
 
-  // Render Stacked Bar Chart
+  const chartColors = {
+    light: {
+      background: "#f4f5f2",
+      text: "#000000",
+      bars: ["#00A3E0", "#FF7A9B", "#FFD700"],
+    },
+    dark: {
+      background: "#f4f5f2",
+      text: "#ffffff",
+      bars: ["#0096D6", "#FF6A85", "#FFC300"],
+    },
+  };
+
+  useEffect(() => {
+    const listener = Appearance.addChangeListener(({ colorScheme }) => {
+      setColorScheme(colorScheme || "light");
+    });
+    return () => listener.remove();
+  }, []);
+
   const renderStackedBarChart = () => {
+    const currentColors = chartColors[colorScheme];
+
     const chartConfig = {
-      backgroundColor: "#ffffff",
-      backgroundGradientFrom: "#f4f5f2",
-      backgroundGradientTo: "#f4f5f2",
+      backgroundColor: currentColors.background,
+      backgroundGradientFrom: currentColors.background,
+      backgroundGradientTo: currentColors.background,
       color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-      labelColor: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+      labelColor: (opacity = 1) => '#8F9BB3',
       barPercentage: 0.5,
-      decimalPlaces: 0, // Optional
+      decimalPlaces: 0,
     };
 
+    const legendData = [
+      { name: "Transportation", color: currentColors.bars[1] },
+      { name: "Food", color: currentColors.bars[0] },
+      { name: "Electricity", color: currentColors.bars[2] },
+    ];
     return (
       <>
-      <View className="">
-        <View className="rounded-3xl  border">
-          
+       <View className="items-center justify-center">
+        <View className="rounded-3xl">
+          <StackedBarChart
+            data={stackedChartData}
+            width={Dimensions.get("window").width - 60}
+            height={220}
+            chartConfig={chartConfig}
+            style={{ borderRadius: 16, }}
+            fromZero
+            hideLegend
+            withHorizontalLabels={false}
+          />
         </View>
-      </View>
-      <View className="items-center">
-      <View className="items-center rounded-3xl  bg-gray-100 w-11/12">
-        <StackedBarChart
-          data={stackedChartData}
-          width={Dimensions.get("window").width - 60}
-          height={220}
-          chartConfig={chartConfig}
-          style={{ borderRadius: 16 }}
-          fromZero
-          hideLegend
-          withHorizontalLabels={false}
-
-        />
-      </View>
+        <View className="flex-row mt-4">
+          {legendData.map((item, index) => (
+            <View key={index} className="flex-row items-center mx-5">
+              <View style={{ width: 16, height: 16, backgroundColor: item.color, marginRight: 4, borderRadius: 90 }} />
+              <Text style={{ fontFamily: 'Poppins-Regular', fontSize: 13, color: currentColors.text }}>
+                {item.name}
+              </Text>
+            </View>
+          ))}
+        </View>
       </View>
       </>
     );
@@ -102,40 +129,90 @@ const ProgressReport = () => {
         </StyledText>
       </StyledLayout>
 
-      <StyledLayout className="flex-1 px-2">
+      <StyledLayout className="flex-1">
         <GoalSetting/>
-        <View className="flex-row h-1/6 justify-center border">
+        <View className="flex-row h-1/4 justify-center mt-3 mb-5">
         {/* <View className="h-full w-full justify-between border flex-row content-start"> */}
-        <DataCard>
-          <Text>Total Impact</Text>
-          <Text>{conditionalConvertGramsToKg(convertTonsToGrams(totalImpact))} of CO₂e</Text>
-        </DataCard>
-        <DataCard>
-          <Text>Initial Emissions</Text>
-          <Text>{(totalEmissions).toFixed(2)} tons of CO₂e</Text>
-          <Text></Text>
-          <Text>
-            Current Emissions
-          </Text>
-          <Text>
-            {(currentFootprint).toFixed(2)} tons of CO₂e
-          </Text>
-        </DataCard>
+          <StyledCard className='h-full w-5/12 p-0 flex mx-2 rounded-3xl' 
+            style={{ 
+              marginRight: 5, 
+              alignItems: 'center', 
+              justifyContent: 'center',
+              borderWidth: 2,
+              borderColor: myTheme['color-success-700'],
+              elevation: 5
+              }}
+            >
+            <Text className="" 
+              style={{ 
+                fontFamily: 'Poppins-Bold', 
+                textAlign: 'center', 
+                justifyContent: 'center', 
+                fontSize: 30, 
+                color: myTheme['color-success-700'],
+                }}
+              >
+              {conditionalConvertGramsToKg(convertTonsToGrams(totalImpact))}<Text style={{ fontFamily: 'Poppins-Regular', fontSize: 17 }}>{"\n"}of CO₂e</Text>
+            </Text>
+            <Text style={{ textAlign: 'center', fontFamily: 'Poppins-Medium', fontSize: 14, color: myTheme['color-basic-600'] }}>Total Impact</Text>
+          </StyledCard>
+
+          <StyledCard className='w-5/12 flex mx-2 rounded-3xl' 
+            style={{ 
+              marginLeft: 5, 
+              alignItems: 'center', 
+              justifyContent: 'center',
+              backgroundColor: myTheme['color-success-transparent-100'],
+              borderColor: myTheme['color-success-700'],
+              }}
+            >
+            <Text className="" 
+              style={{ 
+                fontFamily: 'Poppins-Bold', 
+                textAlign: 'center', 
+                justifyContent: 'center', 
+                fontSize: 20, 
+                color: myTheme['color-success-800'], 
+              }}
+            >
+              {(totalEmissions).toFixed(2)} tons<Text style={{ fontFamily: 'Poppins-Regular', fontSize: 14 }}>{"\n"}of CO₂e</Text></Text>
+            <Text style={{ textAlign: 'center', fontFamily: 'Poppins-Medium', fontSize:12, color: myTheme['color-basic-600']}}>Initial Emissions</Text>
+            <Text></Text>
+            <Text className="" 
+              style={{ 
+                fontFamily: 'Poppins-Bold', 
+                textAlign: 'center', 
+                justifyContent: 'center', 
+                fontSize: 20, 
+                color: myTheme['color-success-800'] 
+              }}
+            >
+              {(currentFootprint).toFixed(2)} tons<Text style={{ fontFamily: 'Poppins-Regular', fontSize: 14 }}>{"\n"}of CO2e per year</Text></Text>
+            <Text style={{ textAlign: 'center', fontFamily: 'Poppins-Medium', fontSize: 12, color: myTheme['color-basic-600']}}>Current Emissions</Text>
+          </StyledCard>
         </View>
         {/* </View> */}
-        <Select
-          value={period}
-          style={{ marginBottom: 10 }}
-        >
-          {["Daily", "Weekly", "Monthly"].map(option => (
-            <SelectItem key={option} title={option} onPress={() => {
-              handlePeriodChange(option);
-              setPeriod(option as "Daily" | "Weekly" | "Monthly")
-            }}/>
-          ))}
-        </Select>
 
-        {renderStackedBarChart()}
+        <StyledCard className="justify-center border rounded-xl mx-1 px-5">
+          <View style={{ flexDirection: 'row', justifyContent: 'space-evenly', marginBottom: 10 }}>
+            {["Daily", "Weekly", "Monthly"].map(option => (
+              <TouchableOpacity key={option} onPress={() => {
+                handlePeriodChange(option);
+                setPeriod(option as "Daily" | "Weekly" | "Monthly");
+              }}>
+                <Text style={{ 
+                  fontFamily: 'Poppins-Medium', 
+                  fontSize: 16, 
+                  color: period === option ? myTheme["color-success-700"] : myTheme["color-basic-600"] 
+                }}>
+                  {option}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </View>
+
+          {renderStackedBarChart()}
+        </StyledCard>
       </StyledLayout>
     </StyledLayout>
   );
