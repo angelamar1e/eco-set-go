e1e6cdbe132d276723fac96f7e1aab13d51316e0
Rianne Maceda <157263717+ri4nne@users.noreply.github.com>
2024-11-03 20:19:49 +0800

    merged with bea

--- types/QuizProps.ts
+++ types/QuizProps.ts
@@ -5,6 +5,9 @@ export interface TemplateProps {
     choices?: Map<string, string> | Map<string, number>;
     defaultValue: any
     onAnswer: (answer: any) => void;
+    isModalVisible: boolean;        // Assuming it's a boolean
+    setModalVisible: (visible: boolean) => void; // Assuming it's a function to set the modal visibility
+    tips: string[]; 
 }
 
 export interface StepperTemplateProps extends TemplateProps {
