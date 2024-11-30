a6177efc3deb3cc10ad4912f9e979e8b3173ac06
Angela <angela.delapena06@gmail.com>
2024-11-30 17:47:16 +0800

    bug fix, null latest goal

--- app/components/(tabs)/Goal Setting/CongratulationsModal.tsx
+++ app/components/(tabs)/Goal Setting/CongratulationsModal.tsx
@@ -37,160 +37,161 @@ const CongratulationsModal: React.FC<CongratulationsModalProps> = ({
     return differenceInDays;
   };
 
-  const difference = getDifferenceInDays(latestGoal!.start_date);
-
-  return (
-    <Modal
-      transparent={true}
-      visible={visible}
-      animationType="fade"
-      onRequestClose={onClose}
-    >
-      <View
-        style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center",
-          backgroundColor: "rgba(0, 0, 0, 0.6)",
-          padding: 20,
-        }}
+  if (latestGoal && latestGoal.start_date) {
+    const difference = getDifferenceInDays(latestGoal!.start_date);
+    return (
+      <Modal
+        transparent={true}
+        visible={visible}
+        animationType="fade"
+        onRequestClose={onClose}
       >
         <View
           style={{
-            backgroundColor: "white",
-            borderRadius: 20,
-            width: "85%",
-            maxWidth: 320,
-            elevation: 8,
-            shadowColor: "#000",
-            shadowOffset: { width: 0, height: 4 },
-            shadowOpacity: 0.3,
-            shadowRadius: 8,
-            overflow: "hidden",
-            justifyContent: "top",
-            minHeight: 270,
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+            backgroundColor: "rgba(0, 0, 0, 0.6)",
+            padding: 20,
           }}
         >
           <View
             style={{
-              backgroundColor: myTheme["color-success-transparent-100"],
-              padding: 12,
-              paddingLeft: 20,
-              flexDirection: "row",
-              // alignItems: "center",
-              marginBottom: 10,
+              backgroundColor: "white",
+              borderRadius: 20,
+              width: "85%",
+              maxWidth: 320,
+              elevation: 8,
+              shadowColor: "#000",
+              shadowOffset: { width: 0, height: 4 },
+              shadowOpacity: 0.3,
+              shadowRadius: 8,
+              overflow: "hidden",
+              justifyContent: "top",
+              minHeight: 270,
             }}
           >
-            <Text
+            <View
               style={{
-                fontFamily: "Poppins-Bold",
-                fontSize: 18,
-                color: myTheme["color-success-800"],
+                backgroundColor: myTheme["color-success-transparent-100"],
+                padding: 12,
+                paddingLeft: 20,
+                flexDirection: "row",
+                // alignItems: "center",
+                marginBottom: 10,
               }}
             >
-              Congratulations! 🥳
-            </Text>
-          </View>
-
-          {/* Content */}
-          <View style={{ padding: 18 }}>
-            <Text
-              style={{
-                fontFamily: "Poppins-Regular",
-                fontSize: 16,
-                color: myTheme["color-basic-800"],
-                lineHeight: 18,
-                marginBottom: 30,
-                textAlign: "justify",
-                paddingHorizontal: 5,
-              }}
-            >
-              You completed your goal{" "}
-              {difference === 0 ? (
-                <Text
-                  style={{
-                    fontFamily: "Poppins-Regular",
-                    fontSize: 16,
-                    color: myTheme["color-basic-800"],
-                    lineHeight: 18,
-                    marginBottom: 16,
-                    textAlign: "justify",
-                    paddingHorizontal: 5,
-                  }}
-                >
-                  within the day. Let's keep up the great work!
-                </Text>
-              ) : (
-                <Text
-                  style={{
-                    fontFamily: "Poppins-Regular",
-                    fontSize: 13,
-                    color: myTheme["color-basic-800"],
-                    lineHeight: 18,
-                    // marginBottom:,
-                    textAlign: "justify",
-                    paddingHorizontal: 5,
-                  }}
-                >
-                  in {difference} days
-                </Text>
-              )}
-            </Text>
-
-            {/* <View className="flex-row justify-center space-"> */}
-              {/* Button */}
-              <TouchableOpacity
-                onPress={onSetNewGoal}
+              <Text
                 style={{
-                  backgroundColor: myTheme["color-success-transparent-100"],
-                  padding: 4,
-                  borderRadius: 10,
-                  alignItems: "center",
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  marginTop: 6,
-                  // marginBottom: 6,
+                  fontFamily: "Poppins-Bold",
+                  fontSize: 18,
+                  color: myTheme["color-success-800"],
                 }}
               >
-                <Text
-                  style={{
-                    fontFamily: "Poppins-SemiBold",
-                    fontSize: 14,
-                    color: myTheme["color-success-800"],
-                  }}
-                >
-                  Set a new goal 🎯
-                </Text>
-              </TouchableOpacity>
-              {/* Button */}
-              <TouchableOpacity
-                onPress={onClose}
+                Congratulations! 🥳
+              </Text>
+            </View>
+  
+            {/* Content */}
+            <View style={{ padding: 18 }}>
+              <Text
                 style={{
-                  backgroundColor: myTheme["color-basic-200"],
-                  padding: 4,
-                  borderRadius: 10,
-                  alignItems: "center",
-                  flexDirection: "row",
-                  justifyContent: "center",
-                  marginTop: 10
+                  fontFamily: "Poppins-Regular",
+                  fontSize: 16,
+                  color: myTheme["color-basic-800"],
+                  lineHeight: 18,
+                  marginBottom: 30,
+                  textAlign: "justify",
+                  paddingHorizontal: 5,
                 }}
               >
-                <Text
+                You completed your goal{" "}
+                {difference === 0 ? (
+                  <Text
+                    style={{
+                      fontFamily: "Poppins-Regular",
+                      fontSize: 16,
+                      color: myTheme["color-basic-800"],
+                      lineHeight: 18,
+                      marginBottom: 16,
+                      textAlign: "justify",
+                      paddingHorizontal: 5,
+                    }}
+                  >
+                    within the day. Let's keep up the great work!
+                  </Text>
+                ) : (
+                  <Text
+                    style={{
+                      fontFamily: "Poppins-Regular",
+                      fontSize: 13,
+                      color: myTheme["color-basic-800"],
+                      lineHeight: 18,
+                      // marginBottom:,
+                      textAlign: "justify",
+                      paddingHorizontal: 5,
+                    }}
+                  >
+                    in {difference} days
+                  </Text>
+                )}
+              </Text>
+  
+              {/* <View className="flex-row justify-center space-"> */}
+                {/* Button */}
+                <TouchableOpacity
+                  onPress={onSetNewGoal}
                   style={{
-                    fontFamily: "Poppins-SemiBold",
-                    fontSize: 14,
-                    color: myTheme["color-basic-600"],
+                    backgroundColor: myTheme["color-success-transparent-100"],
+                    padding: 4,
+                    borderRadius: 10,
+                    alignItems: "center",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    marginTop: 6,
+                    // marginBottom: 6,
                   }}
                 >
-                  Close
-                </Text>
-              </TouchableOpacity>
+                  <Text
+                    style={{
+                      fontFamily: "Poppins-SemiBold",
+                      fontSize: 14,
+                      color: myTheme["color-success-800"],
+                    }}
+                  >
+                    Set a new goal 🎯
+                  </Text>
+                </TouchableOpacity>
+                {/* Button */}
+                <TouchableOpacity
+                  onPress={onClose}
+                  style={{
+                    backgroundColor: myTheme["color-basic-200"],
+                    padding: 4,
+                    borderRadius: 10,
+                    alignItems: "center",
+                    flexDirection: "row",
+                    justifyContent: "center",
+                    marginTop: 10
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontFamily: "Poppins-SemiBold",
+                      fontSize: 14,
+                      color: myTheme["color-basic-600"],
+                    }}
+                  >
+                    Close
+                  </Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-        </View>
-      {/* </View> */}
-    </Modal>
-  );
+        {/* </View> */}
+      </Modal>
+    )
+  }
 };
 
 export default CongratulationsModal;
