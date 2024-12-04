d7b76854241333b91e7298e209d65aeea3f6415f
Angela <angela.delapena06@gmail.com>
2024-12-04 18:28:21 +0800

    fix 'parameterized actions' impact

--- app/components/(tabs)/Home/ParameterizedAction.tsx
+++ app/components/(tabs)/Home/ParameterizedAction.tsx
@@ -3,8 +3,12 @@ import { View } from "react-native";
 import { Checkbox, IconButton } from "react-native-paper";
 import { Swipeable } from "react-native-gesture-handler";
 import { ActionItemProps, DoneItemProps } from "@/types/ActionItemProps";
-import { computeImpact, convertKgToGrams } from '../../../utils/EstimationUtils';
-import { EmissionsContext } from '@/contexts/Emissions';
+import {
+  computeImpact,
+  convertGramsToKg,
+  convertKgToGrams,
+} from "../../../utils/EstimationUtils";
+import { EmissionsContext } from "@/contexts/Emissions";
 import { EmissionsDataContext } from "@/contexts/EmissionsData";
 import { styled } from "nativewind";
 import { Card, Layout, Text } from "@ui-kitten/components";
@@ -22,58 +26,81 @@ export const Parameterized: React.FC<ActionItemProps> = ({
   handleComplete,
   handleDelete,
 }) => {
+  const { emissionsData } = useContext(EmissionsContext);
+  let impact = 0;
+  let baseEmissions;
+  let replacementEmissions;
 
-    const { emissionsData } = useContext(EmissionsContext);
-    let impact = 0;
+  function getImpact() {
+    if (item.baseEmission === "coffeeEmissions") {
+      console.log("YES COFFEE");
+      if (emissionsData["hotDrinksFrequency"]["Coffee"] > 1) {
+      console.log(emissionsData["hotDrinksFrequency"]["Coffee"]);
+        impact =
+          item.impact * convertGramsToKg((emissionsData["hotDrinksFrequency"]["Coffee"] - 1));
+        console.log("IMPACT",impact);
+      } else {
+        impact = 0;
+      }
+    } else {
+      baseEmissions = emissionsData[item.baseEmission!] ?? 0;
+      replacementEmissions = emissionsData[item.impact] ?? item.impact;
 
-    function getImpact() {
-        const baseEmissions = emissionsData[item.baseEmission!] ?? 0;
-        const replacementEmissions = emissionsData[item.impact] ?? item.impact
-
-        if (item.title === "Carpool"){
-          impact = baseEmissions * replacementEmissions;
-        }
-        else{
-          impact = computeImpact(baseEmissions, replacementEmissions);
-        }
-
-        handleComplete(item.id, item.template, convertKgToGrams(impact));
+      if (item.title === "Carpool") {
+        impact = baseEmissions * replacementEmissions;
+      } else {
+        impact = computeImpact(baseEmissions, replacementEmissions);
+      }
     }
+
+    handleComplete(item.id, item.template, convertKgToGrams(impact));
+  }
 
   return (
     <Swipeable
       renderRightActions={() => (
         <View className="flex items-center justify-center ml-2 mr-4">
-          <Ionicons name="trash" size={20} color="red" onPress={() => handleDelete(item.id)} />
-        </View>        
+          <Ionicons
+            name="trash"
+            size={20}
+            color="red"
+            onPress={() => handleDelete(item.id)}
+          />
+        </View>
       )}
     >
-    <StyledLayout className="pt-1 m-1 justify-start items-start">
-    <View className="rounded px-3 border border-gray-200 py-2 w-full" style={{justifyContent: 'center', backgroundColor: myTheme['color-basic-200']}}>
+      <StyledLayout className="pt-1 m-1 justify-start items-start">
+        <View
+          className="rounded px-3 border border-gray-200 py-2 w-full"
+          style={{
+            justifyContent: "center",
+            backgroundColor: myTheme["color-basic-200"],
+          }}
+        >
           <View className="flex-row">
-              <CircularCheckbox
-            status={
-              completedActions.some((action) => action.id === item.id)
-                ? "checked"
-                : "unchecked"
-            }
-            onPress={() => getImpact()}
-          />
+            <CircularCheckbox
+              status={
+                completedActions.some((action) => action.id === item.id)
+                  ? "checked"
+                  : "unchecked"
+              }
+              onPress={() => getImpact()}
+            />
             <StyledText
               numberOfLines={2}
-              style={{ 
+              style={{
                 width: "85%",
-                fontFamily: 'Poppins-Regular',
+                fontFamily: "Poppins-Regular",
                 fontSize: 14,
-                top: 8
+                top: 8,
               }}
-              className="text-base leading-5"
+              className="text-base leading-5 pb-3"
             >
               {item.title}
             </StyledText>
+          </View>
         </View>
-      </View>
-    </StyledLayout>
+      </StyledLayout>
     </Swipeable>
   );
 };
